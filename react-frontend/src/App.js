b35f6fed344d1ff6f71834ee0ea949c9d23fbb78
Guilherme Arnault <guisantos2904@hotmail.com>
2025-10-23 18:17:02 +0100

    TailWind

---
+++ react-frontend/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600"> Tailwind está a funcionar!</h1>
+      <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition">
+        Testar Estilo
+      </button>
+    </div>
+  );
+}
+
+/* function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+} */
+
+export default App;

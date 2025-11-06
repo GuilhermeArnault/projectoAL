c7d1807b116dfa12bfc246aedbd576c6b357c7a6
cunha-afk <bernascunha21@gmail.com>
2025-11-06 17:19:13 +0000

    api

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MeteoController;
+use App\Http\Controllers\ReservaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/public/meteo', [MeteoController::class, 'index']);
+Route::apiResource('reservas', ReservaController::class);
